685a96942ceed6a50699e77d58b3036ff2b22c55
Jordan Gregory <jordangregory@granular.ag>
2019-11-27 01:13:17 -0600

    add aws_s3_bucket template

--- provider.go
+++ provider.go
@@ -1,9 +1,14 @@
 package main
 
-import "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/j4ng5y/terraform-provider-terratest/resources/aws/s3"
+)
 
 func Provider() *schema.Provider {
 	return &schema.Provider{
-		ResourcesMap: map[string]*schema.Resource{},
+		ResourcesMap: map[string]*schema.Resource{
+			"terratest_aws_s3_bucket": s3.ResourceTerratestAWSS3Bucket(),
+		},
 	}
 }
--- resources/aws/s3/resource_terratest_aws_s3_bucket.go
+++ resources/aws/s3/resource_terratest_aws_s3_bucket.go
@@ -1,1 +1,35 @@
 package s3
+
+import "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+
+func ResourceTerratestAWSS3Bucket() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceTerratestAWSS3BucketCreate,
+		Read:   resourceTerratestAWSS3BucketRead,
+		Update: resourceTerratestAWSS3BucketUpdate,
+		Delete: resourceTerratestAWSS3BucketDelete,
+
+		Schema: map[string]*schema.Schema{
+			"resource": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceTerratestAWSS3BucketCreate(d *schema.ResourceData, m interface{}) error {
+	return resourceTerratestAWSS3BucketRead(d, m)
+}
+
+func resourceTerratestAWSS3BucketRead(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func resourceTerratestAWSS3BucketUpdate(d *schema.ResourceData, m interface{}) error {
+	return resourceTerratestAWSS3BucketRead(d, m)
+}
+
+func resourceTerratestAWSS3BucketDelete(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
